7cd9067181f2f9e266f1c38d1bdc642f437b8f8f
Erik Valdivieso <erik.valdivieso@infotec.mx>
2017-02-24 15:39:48 -0600

    Se agrgan rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,49 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { RestorePasswordComponent } from './components/restore-password/restore-password.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login',      component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgotPassword',      component: ForgotPasswordComponent },
+  { path: 'restorePassword', component: RestorePasswordComponent },
+  { path: 'prueba',      component: PruebaComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
+    PruebaComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    RestorePasswordComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
