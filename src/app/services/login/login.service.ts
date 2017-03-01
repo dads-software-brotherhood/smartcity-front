649ee3102a2e3686608a0749ba3425f49f203ddb
Erik Valdivieso <erik.valdivieso@infotec.mx>
2017-03-01 16:55:13 -0600

    Se agrega enviroment para QA y se agregan variables para el servidor del idm

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 
+import { environment } from '../../../environments/environment';
+
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
@@ -35,7 +37,7 @@ export class LoginService {
 
     return this.http
     .post(
-      '/keyrock/v3/auth/tokens',
+      environment.idm_server + '/v3/auth/tokens',
       JSON.stringify(query),
       { headers }
     )
