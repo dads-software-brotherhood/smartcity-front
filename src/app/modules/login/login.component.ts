5470eee97ac2c065375af1296e74417d6dd17fdd
Erik Valdivieso <erik.valdivieso@infotec.mx>
2017-03-01 18:02:51 -0600

    Cambio temporal para el demo

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -27,21 +27,32 @@ export class LoginComponent implements OnInit {
   }
 
   submitForm(form: any) {
-    this.loginService.login(form.email, form.email).subscribe((result) => {
-      if (result) {
-        console.log(result);
+    const user1 = 'karen.najera';
+    const user2 = 'blanca.vazquez';
 
-        if (this.loginService.isLoggedIn()) {
-          this.loginError = false;
-          this.router.navigate(['smart-cities']);
-        } else {
-          this.loginError = true;
+    if ((form.email == user1 && form.email == user1) || (form.email == user2 && form.email == user2) ) {
+      this.loginError = false;
+      this.router.navigate(['smart-cities']);
+    } else {
+      this.loginError = true;
 
-        }
-      } else {
-        this.loginError = true;
-      }
-    });
+    }
+    
+    // this.loginService.login(form.email, form.email).subscribe((result) => {
+    //   if (result) {
+    //     console.log(result);
+    //
+    //     if (this.loginService.isLoggedIn()) {
+    //       this.loginError = false;
+    //       this.router.navigate(['smart-cities']);
+    //     } else {
+    //       this.loginError = true;
+    //
+    //     }
+    //   } else {
+    //     this.loginError = true;
+    //   }
+    // });
   }
 
 }
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  idm_server: "http://207.249.127.180:5000"
+  idm_server: "http://207.249.127.180:5000",
+  client_id: "",
+  client_secret: ""
 };
