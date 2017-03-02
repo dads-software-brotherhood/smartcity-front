f94d9803ede58a2bfaaa124f825f3b58ebd9a6b0
Erik Valdivieso <erik.valdivieso@infotec.mx>
2017-03-02 16:02:58 -0600

    Se agregan datos hardcode para el demo

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -9,19 +9,87 @@ import 'rxjs/add/operator/map';
 export const tokenName = 'x-subject-token';
 const tokenInfoName = 'token-info';
 
+export class Address {
+  country_id: number;
+  region_id: number;
+  location_id: number;
+  postcode: string;
+  street: string;
+  typeAddress: number;
+}
+
 export class UserInfo {
-  name: string;
-  email: string;
-  url: string;
   usr: string;
+
+  name: string;
+  familyName: string;
+
+  birthDate: string;
+  gender: number;
+  email: string;
+
+  addresses: Array<Address>;
 }
 
 const user1 = 'karen.najera';
 const user2 = 'blanca.vazquez';
+const user3 = 'hugo.estrada';
 
 const users: UserInfo[] = [
-  {'name': 'Karen Najera', 'email': 'blanca.vazquez@infotec.mx', 'url': 'http://', 'usr':user1 },
-  {'name': 'Blanca Vazquez', 'email': 'blanca.vazquez@infotec.mx', 'url': 'http://', 'usr':user2 }
+  {
+    'usr': user1,
+    'name': 'Karen',
+    'familyName': 'Najera',
+    'birthDate': '1990-01-01',
+    'gender': 2,
+    'email': 'karen.najera@infotec.mx',
+    'addresses': [
+      {
+        'country_id': 1,
+        'region_id': 1,
+        'location_id': 1,
+        'postcode': '12345',
+        'street': 'Insurgentes 223',
+        'typeAddress': 1
+      }
+    ]
+  },
+  {
+    'usr': user2,
+    'name': 'Blanca',
+    'familyName': 'Vazquez',
+    'birthDate': '1990-01-01',
+    'gender': 2,
+    'email': 'blanca.vazquez@infotec.mx',
+    'addresses': [
+      {
+        'country_id': 1,
+        'region_id': 1,
+        'location_id': 2,
+        'postcode': '12345',
+        'street': 'Tlalpan 101',
+        'typeAddress': 1
+      }
+    ]
+  },
+  {
+    'usr': user3,
+    'name': 'Hugo',
+    'familyName': 'Estrada Esquivel',
+    'birthDate': '1971-04-14',
+    'gender': 1,
+    'email': 'hugo.estrada@infotec.mx',
+    'addresses': [
+      {
+        'country_id': 1,
+        'region_id': 1,
+        'location_id': 3,
+        'postcode': '14050',
+        'street': 'San Fernando 37',
+        'typeAddress': 2
+      }
+    ]
+  }
 ];
 
 @Injectable()
@@ -36,9 +104,9 @@ export class LoginService {
       emailFixed = email.toLowerCase();
     }
 
-    if ((emailFixed == user1 && email == user1) || (emailFixed == user2 && email == user2)) {
-      localStorage.setItem(tokenName, email);
-      localStorage.setItem(tokenInfoName, email);
+    if ((emailFixed == user1 || emailFixed == user2 || emailFixed == user3) && (emailFixed == password)) {
+      localStorage.setItem(tokenName, emailFixed);
+      localStorage.setItem(tokenInfoName, emailFixed);
       return true;
     } else {
       this.logout();
@@ -65,6 +133,8 @@ export class LoginService {
         return users[0];
       } else if (usr == user2) {
         return users[1];
+      } else if (usr == user3) {
+        return users[2];
       } else {
         return null;
       }
