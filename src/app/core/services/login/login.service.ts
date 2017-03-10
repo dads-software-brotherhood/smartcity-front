2f6bc2e6ad1a9f7b60957981c44893f782f75526
Erik Valdivieso <erik.valdivieso@gmail.com>
2017-03-09 22:47:21 -0600

    Update backend url

--- src/app/core/services/login/login.service.ts
+++ src/app/core/services/login/login.service.ts
@@ -46,8 +46,9 @@ export class LoginService {
       return body;
     })
     .catch((error: Response | any) => {
-      // console.log('Error at login');
-      // console.log(error);
+      console.log('Error at login');
+      console.log(error);
+
       this.logout();
       return [{'code': '503'}];
     });
