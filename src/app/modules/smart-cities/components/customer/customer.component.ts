eb03f206845ee00e1de20ec25e96364fc6c7b792
Manuel Gomez <jose.gomez@infotec.mx>
2017-03-16 11:47:28 -0600

    Se sube ejemplo para crud de customers

--- src/app/modules/smart-cities/components/customer/customer.component.ts
+++ src/app/modules/smart-cities/components/customer/customer.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import {RouterLink, Router} from '@angular/router';
 import { Customer } from '../../../../core/models/customer';
 import { CustomerService } from '../../../../core/services/customer/customer.service';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.sass']
+  styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
 
@@ -16,47 +16,48 @@ export class CustomerComponent implements OnInit {
 
   loadingIndicator: boolean = true;
 
-  columns = [
-    { prop: 'id', name: 'ID' },
-    { prop: 'firstName', name: 'Firts name' },
-    { prop: 'lastName', name: 'Last name' }
-  ];
+  // columns = [
+  //   { prop: 'id', name: 'ID' },
+  //   { prop: 'firstName', name: 'Firts name' },
+  //   { prop: 'lastName', name: 'Last name' }
+  // ];
 
-  selected = [];
+  // selected = [];
 
-  constructor(private customerService: CustomerService ) { }
+  constructor(private customerService: CustomerService, private _router: Router ) { }
 
   ngOnInit() {
     this.customerService.getAll().subscribe(
       customers => {
         this.customers = customers;
         this.loadingIndicator = false;
+
       },
       error => this.errorMessage = <any>error
     );
   }
 
-  add() {
-    const names = ['Juan', 'Maria', 'Pedro', 'John', 'Mary', 'Peter'];
-    const lastNames = ['Perez', 'Hernandez', 'Martinez', 'Smith', 'Brown', 'Jones'];
+  // add() {
+  //   const names = ['Juan', 'Maria', 'Pedro', 'John', 'Mary', 'Peter'];
+  //   const lastNames = ['Perez', 'Hernandez', 'Martinez', 'Smith', 'Brown', 'Jones'];
 
-    const customer: Customer = new Customer(names[this.randomNum(0, names.length)], lastNames[this.randomNum(0, lastNames.length)]);
+  //   const customer: Customer = new Customer(names[this.randomNum(0, names.length)], lastNames[this.randomNum(0, lastNames.length)]);
 
-    this.customerService.insert(customer).then(
-      customer => this.customers.push(customer),
-      error =>  this.errorMessage = <any>error
-    );
-  }
+  //   this.customerService.insert(customer).then(
+  //     customer => this.customers.push(customer),
+  //     error =>  this.errorMessage = <any>error
+  //   );
+  // }
 
-  randomNum(start: number, end: number): number {
-    return Math.floor(Math.random() * end) + start;
-  }
+  // randomNum(start: number, end: number): number {
+  //   return Math.floor(Math.random() * end) + start;
+  // }
 
-  onSelect({ selected }) {
-    console.log('Select Event', selected, this.selected);
-  }
+  // onSelect({ selected }) {
+  //   console.log('Select Event', selected, this.selected);
+  // }
 
-  onActivate(event) {
-    console.log('Activate Event', event);
-  }
+  // onActivate(event) {
+  //   console.log('Activate Event', event);
+  // }
 }
--- src/app/modules/smart-cities/smart-cities.module.ts
+++ src/app/modules/smart-cities/smart-cities.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // Eliminar cuando ya no se tenga el componente de prueba
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Eliminar cuando ya no se tenga el componente de prueba
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { SmartCitiesComponent } from './smart-cities.component';
@@ -12,6 +12,7 @@ import { SmartCitiesComponent } from './smart-cities.component';
 import { HomeSmartCitiesComponent } from './components/home-smart-cities/home-smart-cities.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { CustomerFormComponent } from './components/customer/customer-form.component';
 
 // Elementos del template
 import { TemplateModule } from '../../template/template.module';
@@ -28,6 +29,7 @@ import { NotificationTrayModule } from './modules/notification-tray/notification
     NgbModule.forRoot(),
     NgxDatatableModule,
     FormsModule, // Eliminar cuando ya no se tenga el componente de prueba
+    ReactiveFormsModule, //Eliminar cuando ya no se tenga el componente de prueba
     UserAccountModule,
     NotificationTrayModule,
     TemplateModule
@@ -36,6 +38,7 @@ import { NotificationTrayModule } from './modules/notification-tray/notification
     SmartCitiesComponent,
     PruebaComponent, // Se va a borrar
     CustomerComponent, // Se va a borrar
+    CustomerFormComponent, //Se va a borrar
     HomeSmartCitiesComponent
   ],
   exports: [
