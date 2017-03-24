742f1b65f72fc72893ef138ff1027e804df0cdb3
Erik Valdivieso <erik.valdivieso@gmail.com>
2017-03-23 20:34:06 -0600

    Remove unused code

--- src/app/core/services/user-profile/user-profile.service.ts
+++ src/app/core/services/user-profile/user-profile.service.ts
@@ -1,30 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
-import { RemoteConnectionService } from '../remote-connection/remote-connection.service';
 import { LoginService } from '../login/login.service';
-
-import { RemoteUtils } from '../../common/remote-utils';
-import { UserProfile } from '../../models/user-profile';
+import { RemoteConnectionService } from '../remote-connection/remote-connection.service';
 
 import { environment } from '../../../../environments/environment';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
+import { UserProfile } from '../../models/user-profile';
 
 const baseRestPath = '/user-profile';
 const baseGetUserUrl = environment.backend_sdk + baseRestPath;
 
 @Injectable()
-export class UserProfileService extends RemoteUtils {
+export class UserProfileService {
 
-  getUserUrl: string;
+  private getUserUrl: string;
 
-  constructor(private remoteConnectionService: RemoteConnectionService, private loginService: LoginService) {
-    super(loginService);
-
+  constructor(private loginService: LoginService, private remoteConnectionService: RemoteConnectionService) {
     this.getUserUrl = baseGetUserUrl + '/' + loginService.getLoggedUser().id;
   }
 
@@ -34,37 +28,8 @@ export class UserProfileService extends RemoteUtils {
   }
 
   public updateUserProfile(): Observable<boolean> {
-    return this.remoteConnectionService.putAsObservable(this.getUserUrl);
-
+    return null;
+    // return this.remoteConnectionService.putAsObservable(this.getUserUrl);
   }
-
-  //
-  // getUserProfile(token: string, email?: string): Promise<UserProfile> {
-  //   const requestOptions: RequestOptions = this.buildRequestOptions(token, email);
-  //
-  //   return this.http.get(user_profile, requestOptions).toPromise()
-  //     .then((res: Response) => {
-  //       return res.json();
-  //     })
-  //     .catch((error: Response | any) => {
-  //       return null;
-  //     });
-  // }
-  //
-  // private buildRequestOptions(token: string, email?: string):RequestOptions {
-  //   const headers: Headers = new Headers();
-  //   headers.append(constants.authTokenKey, token);
-  //
-  //   const requestOptions: RequestOptions = new RequestOptions();
-  //   requestOptions.headers= headers;
-  //
-  //   if (email) {
-  //     const params: URLSearchParams = new URLSearchParams();
-  //     params.set('email', email);
-  //     requestOptions.search= params;
-  //   }
-  //
-  //   return requestOptions;
-  // }
 
 }
