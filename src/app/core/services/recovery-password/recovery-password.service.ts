6f035ac5337583b2d64b72db428e6472b073e40a
Erik Valdivieso <erik.valdivieso@gmail.com>
2017-03-28 13:39:46 -0600

    Replace recovery password services

---
+++ src/app/core/services/recovery-password/recovery-password.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { LoginService } from '../login/login.service';
+import { RemoteConnectionService } from '../remote-connection/remote-connection.service';
+
+import { environment } from '../../../../environments/environment';
+
+const baseForgotPasswordPath = environment.backend_sdk + '/forgot-password';
+
+@Injectable()
+export class RecoveryPasswordService {
+
+  constructor(private remoteConnectionService: RemoteConnectionService, private loginService: LoginService) {}
+
+  public forgotPassword(email: string): Observable<any> {
+    const url = baseForgotPasswordPath + '/' + email;
+
+    return this.remoteConnectionService.postAsObservable(url)
+    .catch((error) => {
+      return null;
+    });
+  }
+
+}
