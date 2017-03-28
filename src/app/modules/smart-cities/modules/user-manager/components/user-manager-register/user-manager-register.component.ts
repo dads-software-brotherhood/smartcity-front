9b80482c4997865dee501476177303e1a48b83fb
Rolando Cazarez <rolando.cazarez@infotec.mx>
2017-03-28 17:09:25 -0600

    Role inclusion in user-manager-register

--- src/app/modules/smart-cities/modules/user-manager/components/user-manager-register/user-manager-register.component.ts
+++ src/app/modules/smart-cities/modules/user-manager/components/user-manager-register/user-manager-register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EnumEx } from '../../../../../../core/models/EnumEx';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router'
+import { role } from '../../../../../../core/models/role';
 
 @Component({
   selector: 'app-user-register',
@@ -6,10 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-manager-register.component.sass']
 })
 export class UserManagerRegisterComponent implements OnInit {
+  public adminUserRegisterForm : FormGroup;
+  private roles: any[];
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,   
+              private router: Router,
+              private route: ActivatedRoute) { 
+                this.roles = this.getRoles();
+                this.adminUserRegisterForm = fb.group({ //// Make Model driven form
+                      "name": [null, Validators.required],
+                      "familyname": [null, Validators.required],
+                      "email": [null, Validators.required],
+                      "role": [null, Validators.required],
+                      
+                  })
+              }
 
+              private sub: any;
   ngOnInit() {
   }
+  getRoles() {
+    let roles: any[] = [];
 
+    //Get name-value pairs from VehicleTypeEnum
+    let rolesEnumList = EnumEx.getNamesAndValues(role);
+
+    //Convert name-value pairs to VehicleType[]
+    rolesEnumList.forEach(pair => {
+        let role = { 'id': pair.value.toString(), 'name': pair.name };
+        roles.push(role);
+    });
+    return roles;
+}
 }
