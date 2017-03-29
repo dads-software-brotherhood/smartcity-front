16baebbab667bf568d4a68423b405945edc4d299
Erik Valdivieso <erik.valdivieso@gmail.com>
2017-03-28 22:59:37 -0600

    Add component for invalid registration token

--- src/app/modules/signup/signup.module.ts
+++ src/app/modules/signup/signup.module.ts
@@ -5,6 +5,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
 import { SignupComponent } from './signup.component';
+import { AccountVerificationComponent } from './account-verification.component'
 
 // Elementos del template
 import { TemplateModule } from '../../template/template.module';
@@ -19,10 +20,12 @@ import { TemplateModule } from '../../template/template.module';
     TemplateModule
   ],
   declarations: [
-    SignupComponent
+    SignupComponent,
+    AccountVerificationComponent
   ],
   exports: [
-    SignupComponent
+    SignupComponent,
+    AccountVerificationComponent
   ]
 })
 export class SignupModule { }
