026b27143d1db0ed7743a8645b8dd7c6ba385eac
Erik Valdivieso <erik.valdivieso@gmail.com>
2017-03-29 15:44:33 -0600

    add sigup service

---
+++ src/app/core/services/signup/signup.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { RemoteConnectionService } from '../remote-connection/remote-connection.service';
+
+import { environment } from '../../../../environments/environment';
+import { constants } from '../../common/constants';
+
+const registerUrl = environment.backend_sdk + '/register';
+const registerValidTokenBaseUrl = environment.backend_sdk + '/register/validation/';
+
+@Injectable()
+export class SignupService {
+
+  constructor(private remoteConnectionService: RemoteConnectionService) {}
+
+  public register(username: string, password: string): Observable<any> {
+    const payload = {
+      'username': username,
+      'password' : password
+    }
+
+    return this.remoteConnectionService.postAsObservable(registerUrl, JSON.stringify(payload), 'application/json');
+  }
+
+}
