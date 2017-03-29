bc23756e14b0eed5cd712815384174a4aa1dede8
Rolando Cazarez <rolando.cazarez@infotec.mx>
2017-03-29 15:57:14 -0600

    Validations for user-tray

--- src/app/modules/smart-cities/modules/user-manager/components/user-manager-register/user-manager-register.component.ts
+++ src/app/modules/smart-cities/modules/user-manager/components/user-manager-register/user-manager-register.component.ts
@@ -4,6 +4,10 @@ import { EnumEx } from '../../../../../../core/models/EnumEx';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router'
 import { role } from '../../../../../../core/models/role';
+import { CustomValidators } from 'ng2-validation';
+
+import {IdentityUser} from '../../../../../../core/models/identity-user';
+import { LoginService } from '../../../../../../core/services/login/login.service';
 
 @Component({
   selector: 'app-user-register',
@@ -12,17 +16,20 @@ import { role } from '../../../../../../core/models/role';
 })
 export class UserManagerRegisterComponent implements OnInit {
   public adminUserRegisterForm : FormGroup;
+  identityUser: IdentityUser;
+  private userRoles: any[];
   private roles: any[];
   errorMessage: string;
 
   constructor(private fb: FormBuilder,   
               private router: Router,
-              private route: ActivatedRoute) { 
+              private route: ActivatedRoute, private loginService: LoginService) { 
+                
                 this.roles = this.getRoles();
                 this.adminUserRegisterForm = fb.group({ //// Make Model driven form
                       "name": [null, Validators.required],
                       "familyname": [null, Validators.required],
-                      "email": [null, Validators.required],
+                      "email": [null, Validators.compose([Validators.required,CustomValidators.email])],
                       "role": [null, Validators.required],
                       
                   })
@@ -30,6 +37,13 @@ export class UserManagerRegisterComponent implements OnInit {
 
               private sub: any;
   ngOnInit() {
+    var index;
+    this.identityUser = this.loginService.getLoggedUser();
+    console.log(this.loginService.getLoggedUser());
+    for (index = 0; index < this.identityUser.roles.length; ++index) {
+    console.log(this.identityUser.roles[index]);
+}
+    
   }
   getRoles() {
     let roles: any[] = [];
--- src/app/modules/smart-cities/modules/user-manager/components/user-manager-tray/user-manager-tray.component.ts
+++ src/app/modules/smart-cities/modules/user-manager/components/user-manager-tray/user-manager-tray.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { EnumEx } from '../../../../../../core/models/EnumEx';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router'
+import { role } from '../../../../../../core/models/role';
+import { CustomValidators } from 'ng2-validation';
 @Component({
   selector: 'app-user-manager-tray',
   templateUrl: './user-manager-tray.component.html',
   styleUrls: ['./user-manager-tray.component.sass']
 })
 export class UserManagerTrayComponent implements OnInit {
-
-  constructor() { }
+  public UserTrayForm : FormGroup;
+  private roles: any[];
+  private users: any[];
+  errorMessage: string;
+  constructor(private fb: FormBuilder,   
+              private router: Router,
+              private route: ActivatedRoute) { 
+                 this.roles = this.getRoles();
+                this.UserTrayForm = fb.group({ //// Make Model driven form
+                      "name": [null],
+                      "familyname": [null],
+                      "email": [null, Validators.compose([Validators.required,CustomValidators.email])],
+                      "role": [null]
+                      
+                      
+                  })
+              }
 
   ngOnInit() {
   }
+    getRoles() {
+    let roles: any[] = [];
 
+    //Get name-value pairs from VehicleTypeEnum
+    let rolesEnumList = EnumEx.getNamesAndValues(role);
+
+    //Convert name-value pairs to VehicleType[]
+    rolesEnumList.forEach(pair => {
+        let role = { 'id': pair.value.toString(), 'name': pair.name };
+        roles.push(role);
+    });
+    return roles;
+}
+  deleteUser(){
+    var person = prompt("Please enter your name", "");
+if (person != null) {
+   alert(person.toString());
+   }
+  }
 }
