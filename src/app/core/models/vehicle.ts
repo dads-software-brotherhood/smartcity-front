cb36a16816e1ff1a9066ca1d3a10ba3f2f0afd08
Manuel Gomez <jose.gomez@infotec.mx>
2017-03-29 21:59:14 -0600

    Properties are added to the vehicle model and Spelling correction

--- src/app/core/models/vehicle.ts
+++ src/app/core/models/vehicle.ts
@@ -13,12 +13,20 @@ export class Vehicle {
   fuelType: FuelType;
   fuelConsumption: number;
   vehiclPlateIdentifier?: string;
+  manufacturerName?: string;
+  cargoVolume?: number;
+  height?: number;
+  width: number;
+  depth: number;
+  weight: number;
   dateModified?: Date;
   datecreated?: Date;
   
   constructor(index?: string, name?: string, description?: string, vehicleType?: VehicleType,
               brandName?: string, modelName?: string, vehicleModelDate?: Date, fuelType?: FuelType,
-              fuelConsumption?: number, vehiclPlateIdentifier?: string, dateModified?: Date, dateCreated?: Date) {
+              fuelConsumption?: number, vehiclPlateIdentifier?: string, manufacturerName?: string,
+              cargoVolume?: number, height?: number, width?: number, depth?: number, weight?: number,
+              dateModified?: Date, dateCreated?: Date) {
     this.index = index;
     this.name = name;
     this.description = description;
@@ -29,6 +37,12 @@ export class Vehicle {
     this.fuelType = fuelType;
     this.fuelConsumption = fuelConsumption;
     this.vehiclPlateIdentifier = vehiclPlateIdentifier;
+    this.manufacturerName = manufacturerName;
+    this.cargoVolume = cargoVolume;
+    this.height = height;
+    this.width = width;
+    this.depth = depth;
+    this.weight = weight;
     this.dateModified = dateModified;
     this.datecreated = dateCreated;
   }
