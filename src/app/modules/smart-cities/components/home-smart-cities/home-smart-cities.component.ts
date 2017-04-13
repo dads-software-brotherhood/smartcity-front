dc0847255f782c1015fab12433abac0b9dd56b26
Erik Valdivieso <erik.valdivieso@gmail.com>
2017-04-13 00:34:37 -0500

    Update bootstrap version

--- src/app/modules/smart-cities/components/home-smart-cities/home-smart-cities.component.ts
+++ src/app/modules/smart-cities/components/home-smart-cities/home-smart-cities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home-smart-cities',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeSmartCitiesComponent implements OnInit {
 
-  constructor() {
-  }
+  closeResult: string;
+  model;
+
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
