66a2b7ccce4b9e5616231be96433682b8a65bf9e
Manuel Gomez <gmanueluli@gmail.com>
2017-04-24 18:51:00 -0500

    Added functionality to display alert severity description for notifications

--- src/app/modules/smart-cities/modules/notification/components/notification-tray/notification-tray.component.ts
+++ src/app/modules/smart-cities/modules/notification/components/notification-tray/notification-tray.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { RouterLink, Router} from '@angular/router';
 import { Alert } from '../../../../../../core/models/alert';
+import { NotificationType } from '../../../../../../core/models/notification-type';
 import { AlertService } from '../../../../../../core/services/alert/alert.service';
 import { LoginService } from '../../../../../../core/services/login/login.service';
+import { Severity } from '../../../../../../core/models/severity';
+import { EnumEx } from '../../../../../../core/models/EnumEx';
+import { NotificationTypeService } from '../../../../../../core/services/notification-type/notification-type.service';
+
 
 @Component({
   selector: 'user-vehicle-tray',
@@ -20,16 +25,21 @@ export class NotificationTrayComponent implements OnInit {
   private messageModal: string;
   private includeText: boolean;
   private alerts: Alert[] = [];
+  private severities: any[] = [];
   private Objalert = new Alert();
+  private notifications: NotificationType[] = [];
   
   private isUser: boolean = false;
 
-  constructor(private _service: AlertService, private _loginService: LoginService, private _router: Router ) { 
+  constructor(private _service: AlertService, private _loginService: LoginService, 
+              private _router: Router, private _notificationService: NotificationTypeService) { 
   }
 
   ngOnInit() {  
     try
     { 
+      this.getNotification();
+      this.severities = this.getSeverity();
       this.bindTable();
       this.isUser = this._loginService.isUser();
       this.isConfirm = true;
@@ -38,7 +48,7 @@ export class NotificationTrayComponent implements OnInit {
     }
     catch(e)
     {
-      this.setValuesModal("An error occurred while loading the notification list", true, false);
+      this.setValuesModal(this.messageModal, true, false);
     }
   }
   
@@ -47,14 +57,57 @@ export class NotificationTrayComponent implements OnInit {
   bindTable() { 
     try
     {
-      this._service.getAll().subscribe(alerts => { this.alerts = alerts;
-      }, error => this.messageModal = <any>error);
+      let severityIndex: number = 0;
+      this._service.getAll().subscribe(
+        (res) => { 
+          this.alerts = res;
+          for(let i=0; i < this.alerts.length; i++)
+          {
+            severityIndex = this.alerts[i].severity - 1;
+            this.alerts[i].severityDesc = this.severities[severityIndex].name;
+          }
+        },
+        (error) => {
+            this.messageModal = error;
+        });
     }
     catch(e)
     {
       throw e;
     }
   }
+
+  getSeverity() {
+    try
+    {
+        let value: number;
+        let severities: any[] = [];
+        //Obtener pares nombre-valor de SeverityEnum
+        let severityEnumList = EnumEx.getNamesAndValues(Severity);
+        //Convertir los nombres-valores a Severity[]
+        severityEnumList.forEach(pair => {
+          value = pair.value + 1;
+          let severity = { 'id': value.toString(), 'name': pair.name };
+            severities.push(severity);
+        });
+        return severities;
+    }
+    catch(e){throw e;}
+}
+
+  getNotification() {
+     try
+      {
+        this._notificationService.getAll().subscribe(
+        (res) => {
+            this.notifications = res;
+        },
+        (error) => {
+            this.messageModal = error;
+        });
+      }
+      catch(e){ throw e;}
+}
 
   //Metodo que se llama cuando se confirma la eliminación del registro
   confirmDelete() {
@@ -66,7 +119,28 @@ export class NotificationTrayComponent implements OnInit {
         else
           this.showDialog = false;
       }
-      catch(e){}
+      catch(e){ throw e;}
+    }
+
+    OnClear(){
+      var selectNotification, selectSeverity, inputDate;
+      try
+      {
+        selectNotification = document.getElementById("notificationType");
+        selectSeverity = document.getElementById("severity");
+        inputDate = document.getElementById("alertDate");
+
+        if(selectNotification != null && selectSeverity != null && inputDate != null)
+        {
+          (<HTMLSelectElement>selectNotification).options.selectedIndex = 0;
+          (<HTMLSelectElement>selectSeverity).options.selectedIndex = 0;
+          (<HTMLInputElement>inputDate).value = "";
+          (<HTMLInputElement>inputDate).placeholder = "yyyy-mm-dd";
+        }
+      }
+      catch(e){
+          this.setValuesModal("An error occurred while clearing the search controls", true, false);
+      }
     }
 
     setValuesModal(message: string, show: boolean, confirm: boolean)
